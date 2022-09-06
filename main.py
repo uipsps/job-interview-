310379f4cbda52a5811b9adb542f17a141d73ba3
uips <111256281+uipsps@users.noreply.github.com>
2022-09-05 23:29:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print('lets get started with this interview')
+
+name=input('what is your name \n')
+age=input('what age are you ')
+
+yearsxp=input('how many years have you been cooking')
+
+
+if int(age) > 999 and int(yearsxp) > 1:
+  print(name + ' you are hired')
+else:
+    print(name + ' ya failed the interview')
+    exit()
+
+
+
+
+
+
